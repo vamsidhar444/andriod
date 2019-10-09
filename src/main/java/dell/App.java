ff699b9ccdbbad1eac40b52365d90649efb91bf4
vamsidhar444 <53784753+vamsidhar444@users.noreply.github.com>
2019-10-08 23:21:21 -0700

    Update App.java

--- src/main/java/dell/App.java
+++ src/main/java/dell/App.java
@@ -1,10 +1,20 @@
-                                                                                                                                                                }    pa
-
- Hello world!
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+                                                                                                                                                               
+public class Prime {
+    public static void main(String[] args) {
+        int num = 29;
+        boolean flag = false;
+        for(int i = 2; i <= num/2; ++i)
+        {
+            // condition for nonprime number
+            if(num % i == 0)
+            {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
     }
 }
